daded923f37391ff21237e699592ed540a314b57
BobEve <bogoodroid@gmail.com>
2017-12-23 14:30:48 +0800

    remove pm25 key

--- app/src/main/java/bob/eve/pm/app/Constants.java
+++ app/src/main/java/bob/eve/pm/app/Constants.java
@@ -15,11 +15,9 @@ import java.io.File;
  */
 
 public final class Constants {
-	public static final String PM25IN_PUBLIC_KEY = "5j1znBVAsnSf5xQyNQyq";
-	public static final String AQICN_PUBLIC_TOKEN = "dome"; // test
+
 	public static final String AQICN_TOKEN = "21acde2c4ac1fc1f47d4cbeb752e6bc000ea171d";
 	public static final String TIAN_XING_KEY = "965831c7d8400cb5f065e3d783c5296b";
-
 	public static final String BUGLY_APP_ID = "9a7bb55bcb";
 
 	public static final String GIRLS_SDCARD_PATH = Environment.getExternalStorageDirectory()
